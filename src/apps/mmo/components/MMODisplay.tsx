efa2c7e3607646015b22265467ee8cf741897458
David Ayeke <ayekedavid@yahoo.com>
2017-12-15 01:13:50 -0600

    [HotFixed] Removed react-elm and started new project

--- src/apps/mmo/components/MMODisplay.tsx
+++ src/apps/mmo/components/MMODisplay.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Elm from 'react-elm-components';
-import { ElmMMO } from '../elm/mmo_elm.js';
 import { Media } from 'react-bootstrap';
 
 export const MMODisplay: React.SFC<{}> = () => (
@@ -8,7 +6,6 @@ export const MMODisplay: React.SFC<{}> = () => (
         <div style={{height: '120%' }} />
         <Media>
             <Media.Body><p>You Decide To: </p>
-                <Elm src={ ElmMMO } />
             </Media.Body>
         </Media>
     </div>    
